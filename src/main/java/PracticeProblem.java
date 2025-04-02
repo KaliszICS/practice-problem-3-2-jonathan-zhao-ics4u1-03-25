7c783e46fdf4e266f3f79d22cc216a8c348c6139
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-04-02 18:57:08 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,37 @@
-public class PracticeProblem {
+public class PracticeProblem { //change to 1.40.0 version of language support for java
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static int towerOfHanoi(int num) {
+		if (num < 3) {
+			return -1;
+			
+		}
+		return (int) Math.pow(2, num) - 1;
 	}
 
 }
+
+// 1 disc takes = 1
+// 2 discs takes = 3
+// 3 discs takes = 7
+// 4 discs takes = 15
+// 5 discs takes = 31
+
+// 2 
+// 4
+// 8
+// 16
+
+//2x2
+//4x2
+//8x2
+
+// 2^numOfDiscs - 1
+// 1 discs = 2^1 - 1 = 1
+// 2 discs = 2^2 - 1 = 3
+// 3 discs = 2^3 - 1 = 7
+// 4 discs = 2^4 - 1 = 15
+// 5 discs = 2^5 - 1 = 31
